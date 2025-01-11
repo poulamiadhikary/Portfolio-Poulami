c959d26a764d3c43d98a98fe811c66b89d7dba28
Poulami Adhikary <poulamiadhikary18@gmail.com>
2025-01-12 04:58:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',
   plugins: [react()],
   base: '/', // Ensure this is set to the root path
   build: {
